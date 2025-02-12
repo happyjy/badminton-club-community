0eff8c15416649078800cd847079492d26d0492f
happyjy <okwoyjy@gmail.com>
2025-02-13 00:07:13 +0900

    refactor: 회원정보 수정에서 nickname 필드 임시 주석처리

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -130,7 +130,7 @@ function ProfilePage({ user }: ProfilePageProps) {
           )}
         </div>
 
-        <div>
+        {/* <div>
           <label className="block text-sm font-medium text-gray-700 mb-2">
             닉네임
           </label>
@@ -142,7 +142,7 @@ function ProfilePage({ user }: ProfilePageProps) {
             }
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
-        </div>
+        </div> */}
 
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-2">
