9e78ed20b017ac1ded42289156485117c0c8490f
happyjy <okwoyjy@gmail.com>
2025-02-15 14:39:42 +0900

    feat: 구글 메타 데이터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,4 +3,8 @@ export const metadata = {
     index: true,
     follow: true,
   },
+  // Next가 <meta name="google-site-verification content="값" /> 을 만들어줍니다.
+  verification: {
+    google: 'bTqFYbHTfK1wiMqYL3xztX9fmwDIPlxSNi5',
+  },
 };
