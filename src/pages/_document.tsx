96da38df949027384a3d1c4c31fed11e5e31babf
happyjy <okwoyjy@gmail.com>
2025-06-08 01:44:38 +0900

    feat: PWA 테마 색상 변경
    - _document.tsx에서 PWA 테마 색상을 '#4CAF50'에서 '#ffffff'로 변경하여 디자인 일관성 유지

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,7 +14,7 @@ export default function Document() {
         />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="theme-color" content="#4CAF50" />
+        <meta name="theme-color" content="#ffffff" />
 
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
